0e02f689abbeba6fdcc8d62566055e10729515a4
Luc4G3r <47355690+Luc4G3r@users.noreply.github.com>
2021-03-22 14:21:43 +0100

    Create xdebug_disable_globally.sh

---
+++ php_xdebug/xdebug_disable_globally.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat /usr/local/etc/php/conf.d/xdebug.ini.dis | \
+        sudo tee \
+        /etc/php/7.0/mods-available/xdebug.ini \
+        /etc/php/7.1/mods-available/xdebug.ini  \
+        /etc/php/7.2/mods-available/xdebug.ini  \
+        /etc/php/7.3/mods-available/xdebug.ini \
+        /etc/php/7.4/mods-available/xdebug.ini \
+        > /dev/null
+cat /usr/local/etc/php/conf.d/xdebug-56.ini.dis | \
+        sudo tee /etc/php/5.6/mods-available/xdebug.ini \
+        > /dev/null
